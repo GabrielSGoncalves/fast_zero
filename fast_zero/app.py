2d6cb9f2b954917caaae7bcb5239ce2d22d1626d
Gabriel S. Goncalves <gabrielgoncalvesbr@gmail.com>
2024-01-29 23:07:59 -0300

    Update app module

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+from fast_zero.database import get_session
+from fast_zero.models import User
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
@@ -13,12 +17,24 @@ def read_root():
 
 
 @app.post('/users/', status_code=201, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(User.username == user.username)
+    )
 
-    database.append(user_with_id)
+    if db_user:
+        raise HTTPException(
+            status_code=400, detail='Username already registered'
+        )
 
-    return user_with_id
+    db_user = User(
+        username=user.name, password=user.password, email=user.email
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', response_model=UserList)
